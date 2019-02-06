26851e9e94f6fb4fe8deff05e064c9451e57702f
Poly Goveia <poly.goveia@gmail.com>
2019-02-05 22:09:44 -0500

    really finish mvp

--- Article/Article.js
+++ Article/Article.js
@@ -9,7 +9,6 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     
-    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     
@@ -17,7 +16,6 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    console.log('you clicked this');
     this.domElement.classList.toggle('article-open');
   }
 }
